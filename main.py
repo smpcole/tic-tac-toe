c1ef15e895d4f79a9ef5c83aa13964d30bc8dbff
Sam Cole <scole3@uic.edu>
2014-06-11 09:59:46 -0500

    Add main loop
    For now, play with two human players

---
+++ main.py
@@ -0,0 +1,21 @@
+from Board import *
+from Player import *
+
+def main():
+    board = Board()
+    players = (HumanPlayer('x', board), HumanPlayer('o', board))
+    turnNum = 0
+    currentPlayer = None
+    while not board.endGame():
+        currentPlayer = players[turnNum % 2]
+        print "%s's turn" % currentPlayer
+        currentPlayer.takeTurn(board)
+        print board
+        turnNum += 1
+    if board.winner != None:
+        print "%ss win!" % board.winner
+    else:
+        print "It's a tie!"
+
+if __name__ == "__main__":
+    main()
